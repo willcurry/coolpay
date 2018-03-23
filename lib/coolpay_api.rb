e2a1826d855ebf6ddd4fced0bcdbe018d069c126
Will Curry <watercrafthd@gmail.com>
2018-03-23 15:08:20 +0000

    Refactor coolpay api wrapper

--- lib/coolpay_api.rb
+++ lib/coolpay_api.rb
@@ -2,7 +2,8 @@ require 'json'
 require_relative 'http_requests.rb'
 
 class CoolpayAPI
-  BASE_URI = 'https://coolpay.herokuapp.com/api'
+  BASE_URI = "https://coolpay.herokuapp.com/api"
+  CONTENT_TYPES = {:post => "application/x-www-form-urlencoded", :get => "application/json"}
 
   def initialize(http=HTTPRequests.new)
     @http = http
@@ -11,50 +12,45 @@ class CoolpayAPI
   def login
     body = {:username => ENV['COOLPAY_USERNAME'], 'apikey' => ENV['COOLPAY_APIKEY']}
     headers = {"Content-Type" => "application/x-www-form-urlencoded"}
-    post_request("/login", body, headers, "token")
+    response = @http.post(BASE_URI + "/login", body, headers)
+    JSON.parse(response.body)["token"]
   end
 
   def get_all_recipients(token)
-    get_request("/recipients", get_headers(token), "recipients")
+    get_request("/recipients", token, "recipients")
   end
 
   def create_recipient(name, token)
     body = {"recipient" => {"name" => name}}
-    post_request("/recipients", body, post_headers(token), "recipient")
+    post_request("/recipients", body, token, "recipient")
   end
 
   def create_payment(amount, currency, recipient_id, token)
     body = {"payment" => {"amount" => amount, "currency" => currency, "recipient_id" => recipient_id}}
-    post_request("/payments", body, post_headers(token), "payment")
+    post_request("/payments", body, token, "payment")
   end
 
   def get_all_payments(token)
-    get_request("/payments", get_headers(token), "payments")
+    get_request("/payments", token, "payments")
   end
 
   def get_recipient_by_name(name, token)
-    get_request("/recipients?name=#{name}", get_headers(token), "recipients")
+    get_request("/recipients?name=#{name}", token, "recipients")
   end
 
   private
 
-  def post_headers(token)
-    {"Content-Type" => "application/x-www-form-urlencoded", "Authorization" => "Bearer #{token}"}
+  def headers(type, token)
+    {"Content-Type" => CONTENT_TYPES[type], "Authorization" => "Bearer #{token}"}
   end
 
-  def get_headers(token)
-    {"Content-Type" => "application/json", "Authorization" => "Bearer #{token}"}
+  def post_request(route, body, token, index)
+     response = @http.post(BASE_URI + route, body, headers(:post, token))
+     JSON.parse(response.body)[index]
   end
 
-  def post_request(route, body, headers, index)
-     response = @http.post(BASE_URI + route, body, headers)
-     parsed_response = JSON.parse(response.body)
-     parsed_response[index]
-  end
-
-  def get_request(route, headers, index)
-    response = @http.get(BASE_URI + route, headers)
-    parsed_response = JSON.parse(response.body)
-    parsed_response[index]
+  def get_request(route, token, index)
+    response = @http.get(BASE_URI + route, headers(:get, token))
+    JSON.parse(response.body)[index]
   end
 end
